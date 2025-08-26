59c105e2703650b50c4b75699f0ec3c580389574
gajbhiyeGaurav <gajbhiyegaurav409@gmail.com>
2025-08-26 12:33:54 +0530

    Add files via upload

---
+++ theme.ts
@@ -0,0 +1,20 @@
+export const COLORS ={
+    first: "#011627",
+    second: "#fdfffc",
+    threed: "#2ec4b6",
+    fourth: "#e71d36",
+    fifth: "#ff9f1c",
+
+    background: "#000000",
+    surface: "#1A1A1A",
+    surfaceLight: "#2A2A2A",
+    white: "#ffffff",
+    grey: "#9CA3AF",
+} as const;
+
+
+// 011627  // Dark Blue
+// fdfffc  // light cream
+// 2ec4b6  // Bright Blue
+// e71d36  // Bright Red
+// ff9f1c  // Bright Orange
